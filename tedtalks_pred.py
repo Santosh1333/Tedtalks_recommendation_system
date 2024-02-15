4d10e2cabe3deb7af97d1791a382051d97ddc695
Krishna Santosh <88792557+Santosh1333@users.noreply.github.com>
2024-02-15 22:43:13 +0530

    tedtalks_pred.py

--- tedtalks_pred.py
+++ tedtalks_pred.py
@@ -4,7 +4,7 @@ import streamlit as st
 # Attempt to load the saved model
 try:
     with open('tedtalks_recommendation.sav', 'rb') as f:
-        Tedtalks_recommendation_model = pickle.load(f)
+        loaded_model = pickle.load(f)
 except FileNotFoundError:
     st.error("Model file not found. Please ensure that the file path is correct.")
 except Exception as e:
@@ -23,6 +23,6 @@ if st.button('Get Recommendations'):
     # For example, if it's text, you might need to preprocess it before prediction
     # Replace 'input_data' with the correct preprocessing of 'prompt' based on your model's requirements
     
-    tedtalk_recom = Tedtalks_recommendation_model.predict(prompt)
+    tedtalk_recom = loaded_model.predict(prompt)
 
 st.success(tedtalk_recom)
