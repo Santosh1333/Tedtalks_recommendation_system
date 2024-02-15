df583cb846bcfd6369e77a6e8145eb19b2aac7ef
Krishna Santosh <88792557+Santosh1333@users.noreply.github.com>
2024-02-15 22:55:49 +0530

    tedtalks_pred.py

--- tedtalks_pred.py
+++ tedtalks_pred.py
@@ -1,7 +1,7 @@
 import pickle
 import streamlit as st
 
-tedtalks_recommendation_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+tedtalks_recommendation_model = pickle.load(open('tedtalks_recommendation.sav', 'rb'))
 
 st.title('Ted Talks Recommendation')
 
@@ -9,10 +9,10 @@ st.title('Ted Talks Recommendation')
 prompt = st.text_input('Enter your prompt here')
 
 # Recommendation logic
-tedtalk_recom = ''
+
 
 if st.button('Get Recommendations'):
     # Call the loaded function directly with the input prompt
-    tedtalk_recom = loaded_model(prompt)
+    tedtalk_recom = tedtalks_recommendation_model(prompt)
 
 st.success(tedtalk_recom)
