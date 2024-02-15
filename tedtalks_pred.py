f43cd15249ea8ac1490428db2f0eeb43e75b27b6
Krishna Santosh <88792557+Santosh1333@users.noreply.github.com>
2024-02-15 21:01:20 +0530

    tedtalks_pred.py

--- tedtalks_pred.py
+++ tedtalks_pred.py
@@ -2,26 +2,22 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# loading the saved models
-
-
-
+# loading the saved model
 Tedtalks_recommendation_model = pickle.load(open('tedtalks_recommendation.sav', 'rb'))
 
-st.title('Heart Disease Prediction using ML')
+st.title('Ted Talks Recommendation')
 
-col1 = st.columns(1)
-    
-    with col1:
-        prompt = st.text_input('prompt')\
-# code for Prediction
-    tedtalk_recom = ''
-    
-    
-    
-    if st.button('result'):
-        tedtalk_recom = Tedtalks_recommendation_model.predict(prompt)                          
-        
-        
-        
-    st.success(tedtalk_recom)
+# Collect user input
+prompt = st.text_input('Enter your prompt here')
+
+# Prediction logic
+tedtalk_recom = ''
+
+if st.button('Get Recommendations'):
+    # You need to define what kind of input the model expects
+    # For example, if it's text, you might need to preprocess it before prediction
+    # Replace 'input_data' with the correct preprocessing of 'prompt' based on your model's requirements
+    input_data = preprocess_data(prompt)
+    tedtalk_recom = Tedtalks_recommendation_model.predict(input_data)
+
+st.success(tedtalk_recom)
